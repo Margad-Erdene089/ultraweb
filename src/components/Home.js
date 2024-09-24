a2282516ecfaf9a45ba17d89ff62e153d937340f
Margad-Erdene089 <b.margaderdene1231@gmail.com>
2024-09-24 21:16:19 +0800

    slide

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,6 +1,13 @@
  import React from 'react'; 
  import Navbar from './navbar';
  import "../assets/Home.css";
+ import CustomSlider from "../components/custom.slider";
+ const images = [
+  { imgURL: 'together1.jpg', imgAlt: 'Image 1 Description' },
+  { imgURL: 'together2.jpg', imgAlt: 'Image 2 Description' },
+  { imgURL: 'together3.jpg', imgAlt: 'Image 3 Description' },
+  { imgURL: 'together4.jpg', imgAlt: 'Image 4 Description' }
+];
 const Home = () => {
   
 
@@ -9,7 +16,19 @@ const Home = () => {
     <div>
       <Navbar/>
       <div class="main-content">
-      <h1>asdas</h1>
+      <div className="App">
+      <CustomSlider>
+        {images.map((image, index) => {
+          return (
+            <img
+              key={index}
+              src={require(`../assets/image/${image.imgURL}`)} // Use relative path here
+              alt={image.imgAlt}
+            />
+          );
+        })}
+      </CustomSlider>
+      </div>
       </div>
       
      
